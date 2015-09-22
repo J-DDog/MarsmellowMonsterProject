10c4edfef96038215aa2ec70c9011c34e2fe8cf9
Traitortots <jker3169@740s-pl-116.local>
2015-09-22 08:57:18 -0600

    I Added my new displayMonsterGUI method.

--- src/marshmallow/view/MarshmallowOutput.java
+++ src/marshmallow/view/MarshmallowOutput.java
@@ -1,11 +1,18 @@
 package marshmallow.view;
 
+import javax.swing.JOptionPane;
+
 public class MarshmallowOutput
 {
 	public void displayMonsterInfo(String info)
 	{
 		System.out.println("My monster info is: " + info);
 		
+	}
+	
+	public void displayMonsterGUI(String info)
+	{
+		JOptionPane.showMessageDialog(null, "TEST");
 	}
 	
 }
