667ea41dae2340e1cc981ff9ec266e4b560c0208
Traitortots <jker3169@740s-pl-116.local>
2015-09-30 09:35:24 -0600

    added first showMessage method and Added Documentation

--- src/marshmallow/view/MarshmallowPopup.java
+++ src/marshmallow/view/MarshmallowPopup.java
@@ -2,7 +2,22 @@ package marshmallow.view;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Class for outputting using Popups
+ * @author jker3169
+ *
+ */
 public class MarshmallowPopup
 {
-
+	
+	/**
+	 * Used to show a message to the user Via JOptionPane
+	 * @param message The message that you want to display
+	 */
+	public void showMessage(String message)
+	{
+		JOptionPane.showMessageDialog(null,message);
+	}
+	
+	
 }
